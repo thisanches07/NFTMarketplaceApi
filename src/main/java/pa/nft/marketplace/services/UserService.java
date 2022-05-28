3038bb84d0a4d6c06285c4fcd29ccf5a97a31e91
Bruno de Moraes <bruno.moraes@chefsclub.com.br>
2022-05-28 18:45:50 -0300

    add new entity user

--- src/main/java/pa/nft/marketplace/services/UserService.java
+++ src/main/java/pa/nft/marketplace/services/UserService.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
